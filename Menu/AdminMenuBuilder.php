6a971af6523c3f9d3bd74057224d0d3e229b3f65
Gourab Sahu <gourab@mail.uni-paderborn.de>
2020-05-04 22:10:57 +0200

    Remove deprecation for knp_side_menu
    User Deprecated: Passing "false" as the second argument to "Symfony\Component\HttpKernel\Kernel::getBundle()" is deprecated as of 3.4 and will be removed in 4.0.

--- Menu/AdminMenuBuilder.php
+++ Menu/AdminMenuBuilder.php
@@ -17,6 +17,8 @@ use Nfq\AdminBundle\Event\ConfigureMenuEvent;
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Class AdminMenuBuilder
@@ -26,18 +28,25 @@ class AdminMenuBuilder implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
+    public function __construct(FactoryInterface $factory,EventDispatcherInterface $dispatcher, RequestStack $requestStack)
+    {
+        $this->factory = $factory;
+        $this->dispatcher = $dispatcher;
+        $this->requestStack = $requestStack;
+    }
+
     /**
      * {@inheritdoc}
      */
-    public function buildSideMenu(FactoryInterface $factory)
+    public function buildSideMenu(array $options)
     {
-        $menu = $factory->createItem('root');
+        $menu = $this->factory->createItem('root');
         $menu->setChildrenAttribute('class', 'nav');
         $menu->setChildrenAttribute('id', 'side-menu');
 
-        $this->container->get('event_dispatcher')->dispatch(
+        $this->dispatcher->dispatch(
             ConfigureMenuEvent::SIDE_MENU,
-            new ConfigureMenuEvent($factory, $menu, $this->getCurrentRequest())
+            new ConfigureMenuEvent($this->factory, $menu, $this->getCurrentRequest())
         );
 
         $this->orderMenuItems($menu);
@@ -53,7 +62,7 @@ class AdminMenuBuilder implements ContainerAwareInterface
         $menu = $factory->createItem('root');
         $menu->setChildrenAttribute('class', 'dropdown-menu dropdown-user');
 
-        $this->container->get('event_dispatcher')->dispatch(
+        $this->dispatcher->dispatch(
             ConfigureMenuEvent::HEADER_MENU,
             new ConfigureMenuEvent($factory, $menu, $this->getCurrentRequest())
         );
@@ -66,7 +75,7 @@ class AdminMenuBuilder implements ContainerAwareInterface
      */
     private function getCurrentRequest()
     {
-        return $this->container->get('request_stack')->getCurrentRequest();
+        return $this->requestStack->getCurrentRequest();
     }
 
     /**
