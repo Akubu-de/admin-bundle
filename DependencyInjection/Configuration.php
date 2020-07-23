fc5de00fbfb9f2047624270afb075c484276a95c
Gourab Sahu <gourab@mail.uni-paderborn.de>
2020-07-23 18:56:26 +0200

    Remove rootNode deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -27,8 +27,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('nfq_admin');
+        $treeBuilder = new TreeBuilder('nfq_admin');
         return $treeBuilder;
     }
 }
